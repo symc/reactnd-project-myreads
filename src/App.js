1dc211a1b4e652425162133a5fd1f1b538ab8ef9
Sina Y. Caliskan <sina.yamac@gmail.com>
2017-10-27 19:55:18 -0400

    feat: Add SearchBooks component
    Add the first version (static) of the SearchBooks component. This
    component encapsulates the search page for books

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Bookshelf from './Bookshelf'
 import { Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import SearchBooks from './SearchBooks'
 
 class BooksApp extends React.Component {
   render() {
@@ -77,24 +78,8 @@ class BooksApp extends React.Component {
           </div>
         )}/>
         <Route exact path='/search' render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to='/'>Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
+          <div>
+            <SearchBooks />
           </div>
         )}/> 
       </div>
